7fc3cff18d4350afe64924a0d58d1ca64572688b
corinne <cdallongeville22@gmail.com>
2019-11-25 08:32:53 +0100

    2eme version

--- public/index.php
+++ public/index.php
@@ -1,20 +1,23 @@
 <?php
 class Speedometer
 {
+    const MILES=0.621;
+    const KM=1.609;
 
-    public static function convertKmToMiles($valeur1)
+    public static function convertKmToMiles($valeur)
     {
-        return round(($valeur1*0.621371), 2);
+        return round(($valeur*=self::MILES), 2);
     }
 
-    public static function convertMilesToKm($valeur2)
+    public static function convertMilesToKm($valeur)
     {
 
-        return round(($valeur2/0.621371), 2);
+        return round(($valeur*=self::KM), 2);
     }
 
 
 }
-//en dehors de la classe //
+
+//en dehors de la classe//
 echo '10 KM/H correspond à '. Speedometer::convertKmToMiles(10). 'Miles' . '<br />';
 echo '6,21 Miles correspond à '. Speedometer::convertMilesToKm(6.21). 'Km/H' . '<br />';
